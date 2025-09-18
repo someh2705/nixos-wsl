18ecaa67d5093ef08c3e13f13eaed36095dda9a5
someh2705 <someh2705@gmail.com>
2025-09-18 01:50:44 +0000

    initial commit

---
+++ flake.nix
@@ -0,0 +1,61 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, nixos-wsl, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+
+      system-module = { pkgs, ... }: {
+        nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+        environment.systemPackages = with pkgs; [
+          git
+
+          # lsp
+          nil
+          nixd
+        ];
+
+        users.users.nixos.shell = pkgs.nushell;
+
+        system.stateVersion = "25.05";
+      };
+
+      nixos-wsl-module = { ... }: {
+        wsl = {
+          enable = true;
+          defaultUser = "nixos";
+        };
+      };
+      
+    in {
+      nixosConfigurations = {
+        nixos = nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit home-manager; };
+
+          modules = [
+            system-module
+            
+            nixos-wsl.nixosModules.wsl
+            nixos-wsl-module
+
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.users.nixos = import ./home.nix;
+            }
+          ];
+        };
+      };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,46 @@
+{ pkgs, ... }:
+
+{
+  home.stateVersion = "25.05";
+  
+  home.packages = with pkgs; [
+    git
+    
+    # shell
+    nushell
+    zoxide
+    starship
+
+    # editor
+    helix
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "someh2705";
+    userEmail = "someh2705@gmail.com";
+  };
+
+  programs.nushell = {
+    enable = true;
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableNushellIntegration = true;
+  };
+
+  programs.starship = {
+    enable = true;
+    enableNushellIntegration = true;
+  };
+
+  programs.helix = {
+    enable = true;
+    defaultEditor = true;
+    settings = {
+      theme = "onelight";
+      editor.true-color = true;
+    };
+  };
+}
