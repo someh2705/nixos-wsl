e8a0335c817d74d0990cdefb5adb876b3c4d23c2
someh2705 <someh2705@gmail.com>
2025-09-28 01:05:15 +0000

    move home-manager from 25.05 to unstable

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
